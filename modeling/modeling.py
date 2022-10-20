71a5a3531415234ad5b03c4a80c60d611fd64585
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-10-20 16:29:41 +1000

    Update modeling.py

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -19,13 +19,21 @@ from sklearn.cluster import KMeans, MeanShift, DBSCAN, AgglomerativeClustering
 # TODO: determine whether to focus solely on hierarchical approaches
 # TODO: determine whether NeighborNet is appropriate and how to support it
 
+from .util import get_model, get_method_object
+# TODO: get_method_object should contain objects wrapping external libraries to provide common interface
+
 # TODO: code to determine which words to consider
-def obtain_classifier_words(corpus):
+# TODO: will need to develop polysyllabic word support for Hmong and Chinese
+def obtain_classifier_words(corpus, nom_clf):
     pass
 
 # TODO: code to obtain embedding vectors for each word
-def obtain_vectors(model):
+#  should return dictionary of word : index relations and
+#  vectors from the model
+def obtain_vectors(model, vocab):
     pass
+
+# TODO: define 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -33,12 +41,21 @@ if __name__ == '__main__':
     # TODO: determine whether there should be a n_clusters argument
     # TODO: determine which other hyperparams are necessary
     parser.add_argument('--language', type=str, default='Hmong')
+    parser.add_argument('--nom_clf', type=str, default='lub')
+    parser.add_argument('--corpus', type=str, default='sch')
     parser.add_argument('--model_type', type=str, default='Word2Vec')
-    parser.add_argument('--method', type=str, default='K-Means')
+    parser.add_argument('--cluster_method', type=str, default='K-Means')
     args = parser.parse_args()
     
-    # TODO: call training on method
+    vocab = obtain_classifier_words(args.corpus, args.nom_clf)
+    
+    indices, vectors = obtain_vectors(model, vocab)
+    
+    c_method = get_method_object(args.cluster_method)
+    # TODO: determine what hyperparameters need to be present for train call
+    c_method.train(vectors)
     
     # TODO: call extraction of groups from method object
+    groups = c_method.get_groups()
     
     # TODO: evaluate against relevant Net (HmongNet, Chinese, Japanese WordNet versions)
