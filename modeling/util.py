a440983749655c98c55baded176152076b0edf35
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-10-20 16:31:30 +1000

    Create util.py

---
+++ modeling/util.py
@@ -0,0 +1,19 @@
+# Classifier semantic modeling
+# Original code Copyright © 2022 Nathan M. White
+
+# Copyright notice
+__copyright__ = "Copyright © 2022 Nathan M. White"
+
+# author
+__author__ = "Nathan M. White"
+__author_email__ = "nathan.white1@jcu.edu.au"
+
+__description__ = """This file contains the code for utility functions necessary for ancillary processing for modeling.py."""
+
+# TODO
+def get_model(model_name):
+    pass
+
+# TODO
+def get_method_object(method_name):
+    pass
