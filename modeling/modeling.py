837150c072a1b1dc486be704240b6c3284a79632
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-10-29 19:48:11 +1000

    Update modeling.py

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -13,6 +13,8 @@ __description__ = """This file contains the code for clustering on the model rep
 import argparse
 import os
 
+import xml.etree.ElementTree as ET
+
 from sklearn.cluster import KMeans, MeanShift, DBSCAN, AgglomerativeClustering
 # TODO: determine approach for silhouette analysis with KMeans
 # TODO: determine whether to pursue support for Expectation Maximization via Gaussian Mixture Models
@@ -25,8 +27,28 @@ from .util import get_model, get_method_object
 # TODO: code to determine which words to consider
 # TODO: will need to develop polysyllabic word support for Hmong and Chinese
 # TODO: this should essentially be able to crawl over the WordNet versions for each language
-def obtain_classifier_words(corpus, nom_clf):
-    pass
+# HmongNet directly supports categories, others may not do so
+# TODO: review best placement of the following dictionary
+HMONG_CLF = {'tus': ['person', 'one_dimensional_object', 'animal', 'wild_animal', 'domestic_animal', 'social_role',\
+                     'family_relation', 'consanguineal_relation', 'affinal_relation', 'ethnicity', 'symbol']}
+
+def obtain_classifier_words(corpus, nom_clf, prefix=None):
+    if corpus == 'HmongNet':
+        classifier_words = []
+        xml_data = ET.parse(prefix + 'hmong_ontology.xml')
+        root = xml_data.get_root()
+        # root > word_set > lemma > form > sense > category -- match category, then get form
+        word_set = root[1]
+        for lemma in word_set:
+            for child in lemma:
+                if child.tag == 'form':
+                    form = child.text
+                if child.tag == 'sense':
+                    for c in child:
+                        if c.tag == 'category':
+                            if c.text in HMONG_CLF[nom_clf]:
+                                classifier_words.append(form)
+   return clsasifier_words
 
 # TODO: code to obtain embedding vectors for each word
 #  should return dictionary of word : index relations and
